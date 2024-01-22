e284f977347adb150dfa04a3d663fcc2c0c55b09
ikari2k <pluta.m@gmail.com>
2024-01-22 16:26:18 +0100

    ready for heroku deployment

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import functions
 
+st.set_page_config(page_title='Ikari2k Todo App', layout="wide")
 todos = functions.get_todos()
 
 def add_todo():
@@ -10,8 +11,10 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo list:")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>",unsafe_allow_html=True)
 
+st.text_input(label="New todo" , placeholder="Add new todo...", 
+              label_visibility="hidden", on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,9 +23,6 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-
-st.text_input(label="New todo" , placeholder="Add new todo...", 
-              label_visibility="hidden", on_change=add_todo, key='new_todo')
 
 
 #st.session_state
